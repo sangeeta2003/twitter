f0b0f1b9ff69242dd316c723cc449196143efcfe
*pknowledge* <mishrasangeeta944@gmail.com>
2024-03-24 18:26:16 +0530

    authentication check

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,25 +1,26 @@
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config({
-    path: '../config/.env'
+  path: "../config/.env",
 });
 
-const isAuthenticated =async(req,res,next)=>{
-    try{
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const { token } = res.cookies;
+    console.log(token); 
 
-        const {token} = res.cookies;
-        console.log(token);
-
-        if(!token){
-            return res.status(401).json({
-                message:"User not authenticated",
-                success:false
-            })
-        }
-        const decode = await jwt.verify(token,process.env.TOKEN_SECRET)
-
-    } catch(error){
-console.log(error)
+    if (!token) {
+      return res.status(401).json({
+        message: "User not authenticated",
+        success: false,
+      });
     }
-}
+    const decode = await jwt.verify(token, process.env.TOKEN_SECRET);
+    console.log(decode);
+    req.user = decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { Register,Login ,logout} from "../controllers/userController.js";
+import { Register, Login, logout } from "../controllers/userController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/register').post(Register);
-router.route('/login').post(Login);
-router.route('/logout').get(logout);
+router.route("/register").post(Register);
+router.route("/login").post(Login);
+router.route("/logout").get(logout);
 
-
-export default router
+export default router;
