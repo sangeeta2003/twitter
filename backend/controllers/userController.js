bb1e3521c2ebbae6d46fcc61e75f0081ee8a0663
*pknowledge* <mishrasangeeta944@gmail.com>
2024-03-24 23:25:25 +0530

    api for getprofile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -112,3 +112,20 @@ export const bookmark = async (req, res) => {
     console.log(error);
   }
 };
+
+
+export const getMyProfile = async(req,res) =>{
+
+try{
+const id = req.params.id;
+const user = await User.findById(id).select("-password");
+return res.status(200).json({
+    user,
+})
+
+
+
+}catch(error){
+console.log(error)
+}
+}
