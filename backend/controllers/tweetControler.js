8ad1daa9e5c6f16817904018f6d693ed4ceefb52
*pknowledge* <mishrasangeeta944@gmail.com>
2024-03-24 21:12:29 +0530

    like or dislike api

--- backend/controllers/tweetControler.js
+++ backend/controllers/tweetControler.js
@@ -19,3 +19,40 @@ export const createTweet = async (req, res) => {
     });
   } catch {}
 };
+
+export const deleteTweet = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Tweet.findByIdAndDelete(id);
+    return res.status(401).json({
+      message: "Tweet deleted successfully.",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const likeOrDislike = async (req, res) => {
+  try {
+    const loggedInId = req.body.id;
+    const tweetId = req.params.id;
+
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+      return res.status(404).json({ message: "Tweet not found" });
+    }
+
+    if (tweet.like.includes(loggedInId)) {
+      // Dislike
+      await Tweet.findByIdAndUpdate(tweetId, { $pull: { like: loggedInId } });
+      return res.status(200).json({ message: "User disliked the tweet" });
+    } else {
+      // Like
+      await Tweet.findByIdAndUpdate(tweetId, { $push: { like: loggedInId } });
+      return res.status(200).json({ message: "User liked the tweet" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/tweetRoutes.js
+++ backend/routes/tweetRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 
-import { createTweet } from "../controllers/tweetControler.js";
+import { createTweet, deleteTweet, likeOrDislike } from "../controllers/tweetControler.js";
 import isAuthenticated from "../config/auth.js";
 const router = express.Router();
 
 router.route("/create").post(isAuthenticated, createTweet);
+router.route("/delete/:id").delete(deleteTweet)
+router.route("/like/:id").put(isAuthenticated,likeOrDislike)
+
 export default router;
